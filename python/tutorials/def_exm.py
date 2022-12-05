27570e9e03ffc016a54f584c9007bc90d41d732c
purpl3-yt <remezdevid321@gmail.com>
2022-12-05 16:04:18 +0200

    add tutorials

---
+++ python/tutorials/def_exm.py
@@ -0,0 +1,41 @@
+def Example_Function():
+    print('Hello World!')
+
+Example_Function() # This function print "Hello World!"
+# For example let's create simple calc function
+
+def Calc(a,b): # This function return A+B
+    return a+b # Return
+
+# Let's call it
+
+Calc(2,2) # It return 4
+
+# To display this function use print function
+print(Calc(2,2))
+
+# Also you can use "eval" function, but it's not secure
+# For example, let's calculate 2+2 using the eval function
+
+eval('2+2') # It return 4 but it's not safe because you can enter malicious code
+
+# Let's look at global variables and local
+
+a = 10 # It's global variable
+
+def Function():
+    a = 0 # This is a local variable we can't use it outside of def
+    
+    # But to remove it, just write the word "global"and variables at the beginning of def
+
+# Example
+
+x = 5 # Global var
+
+def Global_Vars():
+    global x # Now we are working with a global variable
+
+    x-=3 # X minus 3
+
+# If we print it it return 2
+print(x)
---
+++ python/tutorials/lambda.py
@@ -0,0 +1,25 @@
+# Lambda is an inline function
+
+# For example let's create simple lambda
+
+lam = lambda a,b : a # It return "a" variable
+
+print(lam(10,20))# Print 10
+
+# But now let's try to make a lambda that will count numbers
+
+count = lambda a,b : a+b # It return a+b variables
+
+print(count(10,20)) # Print 30
+
+'''
+plus = lambda a,b : a+b
+minus = lambda a,b : a-b
+multiply = lambda a,b : a*b
+divide = lambda a,b: a/b
+
+print(plus(2,2))#Integer
+print(minus(10,5))#Integer
+print(multiply(2,2))#Integer
+print(divide(10,2))#Float
+'''
