4af90cde0c1e9acc6807d99aa1e05c79b14ff17f
purpl3-yt <remezdevid321@gmail.com>
2022-12-06 17:49:24 +0200

    fixed

--- python/Win32Clipboard.py
+++ python/Win32Clipboard.py
@@ -7,8 +7,6 @@ def pause():
 if platform.system().lower() != 'windows':
     print("This program doesn't work in linux")
 
-win32clipboard.OpenClipboard()
-
 while True:
     os.system('cls')
     choice = input('''
@@ -17,11 +15,11 @@ while True:
 Select: ''')
 
     if choice == '1':
+        win32clipboard.OpenClipboard()
         clipboard = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
         print('\n'+clipboard)
         pause()
 
     else:
         break
-
-win32clipboard.CloseClipboard()
--- python/tutorials/README.md
+++ python/tutorials/README.md
@@ -1,2 +1,2 @@
-# Python tutorial 
+# Python tutorials
 ## Yes, I know they are badly done, it's just that I've never done tutorials before
